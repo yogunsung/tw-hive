d0033e3722668ec01fafc1f8140ec8f6235f65a7
yogunsung <syougang@gmail.com>
2015-02-04 21:42:08 +0800

    测试连接hiveserver2

---
+++ src/main/java/cn/com/talkweb/hive/jdbc/JdbcHiveServer2.java
@@ -0,0 +1,18 @@
+/** 
+ * Project Name:tw-hive 
+ * File Name:JdbcHiveserver2.java 
+ * Package Name:cn.com.talkweb.hive.jdbc 
+ * Date:2015年2月4日 下午9:01:46 
+ * Copyright (c) 2015, TalkWeb All Rights Reserved. 
+ * 
+ */  
+package cn.com.talkweb.hive.jdbc;
+
+/** 
+ * @author songyg 
+ * @version  
+ * @since JDK 1.6 
+ */
+public class JdbcHiveServer2 {
+
+}
