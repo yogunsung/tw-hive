ddafc81a4b689fa66fb863d0bc8f6d61bfc432c2
yogunsung <syougang@gmail.com>
2015-02-07 18:06:32 +0800

    初始化提交

---
+++ src/main/java/cn/com/talkweb/hive/jdbc/util/HiveOps.java
@@ -0,0 +1,18 @@
+/** 
+ * Project Name:tw-hive 
+ * File Name:HiveOps.java 
+ * Package Name:cn.com.talkweb.hive.jdbc.util 
+ * Date:2015年2月5日 上午8:57:30 
+ * Copyright (c) 2015, TalkWeb All Rights Reserved. 
+ * 
+ */  
+package cn.com.talkweb.hive.jdbc.util;
+
+/** 
+ * @author songyg 
+ * @version  
+ * @since JDK 1.6 
+ */
+public class HiveOps {
+	
+}
